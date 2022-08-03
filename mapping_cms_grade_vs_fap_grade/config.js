1bec7b1c0f57c7959093b3df78619b47ceae231a
Phạm Quang Khang <20201841+khang21081995@users.noreply.github.com>
2022-08-03 12:14:40 +0700

    Update config.js

--- mapping_cms_grade_vs_fap_grade/config.js
+++ mapping_cms_grade_vs_fap_grade/config.js
@@ -1,21 +1,23 @@
 module.exports = {
     FAP_CONFIG: {
-        path: "./Fap_input/IS1413_PRX301_KHANGPQ3_SPRING2022.xlsx",
+        path: "C:\\Users\\khang\\Downloads\\PRF\\PRF192.M5_PRF192_KHANGPQ3_SUMMER2022.xlsx",
         sheetName: "Sheet1",
         bonus_col: "A",
         total_bonus_col: "B",
         member_code_col: "E",
-        raw_grade_col: "L",
-        grade_after_bonus_col: "K",
-        header_row: 1
+        raw_grade_col: "AE",
+        grade_after_bonus_col: "AF",
+        header_row: 1,
+        bonus_scale: 5/1.3 // ví dụ: điểm bonus lấy đầu điểm 5% là gốc, đang tính điểm đầu 10% thì bonus scale là 0.5
     },
     CMS_CONFIG: {
-        path: "./CMS_input/prx_ass1.xlsx",
-        sheetName: "SHEET 1",
-        email_col: "A",
-        grade_col: "B",
+        path: "C:\\Users\\khang\\Downloads\\CMS_EXPORT\\PE_PRF192_Trial_666879_old.xlsx",
+        sheetName: "Result",
+        email_col: "B",
+        grade_col: "F",
         header_row: 1,
-        grade_scale: 0.1 // lấy điểm thực tế trên cms nhân hệ số để ra điểm thang 10 hoặc thang đo muốn hiển thị
+        grade_scale: 1 // lấy điểm thực tế trên cms nhân hệ số để ra điểm thang 10 hoặc thang đo muốn hiển thị
     },
-    saveBackToInput: false
+
+    saveBackToInput: true
 }
